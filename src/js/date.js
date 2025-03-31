eb7795db5d86fc6dad85bd21f6a015fb81bd5e0d
Oleksandr Mil <oleksandr.myliutin@gmail.com>
2025-03-31 20:14:59 +0300

    add new partial 'new-scgedule.html', also add new scss 'new-shedule.scss' and add js-file 'date.js'

--- src/js/date.js
+++ src/js/date.js
@@ -1,18 +1,38 @@
-
-const now = new Date();
-const options = {
-    weekday: 'short',
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    hour12: true,
-};
-
-now.toLocaleString('en-US', options);
-
-const date = document.getElementById('wrapper-note-form-group-calendar');
 document.addEventListener('DOMContentLoaded', () => {
-    date.target.value = now.toLocaleString('en-US', options);
+    const dateStart = document.getElementById('StartFrom');
+    const dateFinish = document.getElementById('FinishFrom');
+    const input = document.getElementById('inputQuestion');
+    const button = document.getElementById('SubmitBtn');
+    const question = document.getElementById('question');
+    const tracking = document.querySelectorAll('.tracking');
+    const now = new Date();
+    const options = {
+        weekday: 'short',
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+    };
+    let wasSubmitted = false;
+    const customDate = new Date(now);
+    button.addEventListener('click', () => {
+        customDate.setDate(now.getDate() + Number(input.value))
+        question.style.display = 'none';
+        tracking.forEach((track) => {
+            track.style.display = 'flex';
+        })
+    });
+    
+    dateStart.addEventListener('click', (e) => { 
+        e.preventDefault();
+        e.currentTarget.textContent = now.toLocaleString('en-US', options);
+    });
+    dateFinish.addEventListener('click', (e) => {
+        e.preventDefault();
+        e.currentTarget.textContent = customDate.toLocaleString('en-US', options);
+    })
+
+    if (wasSubmitted) {
+        
+    }
 });
+
